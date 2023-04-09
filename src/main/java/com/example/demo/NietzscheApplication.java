1eecf2939732ee91acb1f681bb669cd10385762f
unknown <dr.tokudamakoto@icloud.com>
2023-04-09 11:59:07 +0900

    Nietzscheプロジェクトの初コミット

---
+++ src/main/java/com/example/demo/NietzscheApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NietzscheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NietzscheApplication.class, args);
+	}
+
+}
